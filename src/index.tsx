7655612747e6406b9bf819237ebcc06cd0c5eb46
Vineet Sridhar <vineets1600@gmail.com>
2020-11-02 18:53:24 -0500

    Updated icons on tabs

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,31 @@ import ScanScreen from './ScanScreen';
 import React from 'react'
 import ProfileScreen from './ProfileScreen';
 import ContactScreen from './ContactScreen';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
-        <Tab.Screen name="Settings" component={ProfileScreen} />
+    <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+            let iconName:string;
+    
+            if (route.name === 'Scan') {
+                iconName = 'md-qr-scanner';
+            } else if (route.name === 'Profile') {
+                iconName = 'md-person';
+            } else {
+                iconName = "md-people"
+            }
+    
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })
+    
+    }>
+        <Tab.Screen name="Profile" component={ProfileScreen} />
         <Tab.Screen name="Scan" component={ScanScreen} />
         <Tab.Screen name="Contacts" component={ContactScreen} />
     </Tab.Navigator>
