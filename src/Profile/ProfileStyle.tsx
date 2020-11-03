25de173750f0fa4d927a2386f3652b037e663084
ChrisMazzei <chrismazzei213@gmail.com>
2020-11-03 17:45:31 -0500

    Adjusted container to centered, added row container for school name and email address, added styling for TextInput boxes

--- src/Profile/ProfileStyle.tsx
+++ src/Profile/ProfileStyle.tsx
@@ -3,8 +3,27 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
     container:{
         display:'flex',
-        margin: 16
-    }
+        margin: 0,
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center'
+    },
+    flexCode: {
+	flex: 1
+    },
+    rowContainer: {
+    	flex: 1,
+	flexDirection: 'row',
+	justifyContent: 'space-between'
+    },
+    schoolStyle: {
+	textAlign: 'left',
+	height: 50
+    },
+    emailStyle: {
+	textAlign: 'right',
+	height: 50
+    },
 })
 
 export default styles
