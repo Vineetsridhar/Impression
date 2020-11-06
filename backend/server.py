9f69b1cbcb4a04c675e9a2782033863d3f7c6204
EC2 Default User <ec2-user@ip-172-31-11-184.ec2.internal>
2020-11-06 19:54:03 +0000

    inital server.py file

---
+++ backend/server.py
@@ -0,0 +1,11 @@
+# app.py
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
+import datetime
+import flask
+import flask_sqlalchemy
+import flask_socketio
+
+################################
+
