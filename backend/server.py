84f10161b23ef63c320e72fc5d665cc91340598b
EC2 Default User <ec2-user@ip-172-31-11-184.ec2.internal>
2020-11-06 20:58:42 +0000

    connection database access functions

--- backend/server.py
+++ backend/server.py
@@ -7,5 +7,44 @@ import flask
 import flask_sqlalchemy
 import flask_socketio
 
+import tables
+
 ################################
 
+# Configuration and Variables
+
+app = flask.Flask(__name__)
+
+socketio = flask_socketio.SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = TODO
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.init_app(app)
+db.app = app
+db.create_all()
+db.session.commit()
+
+################################
+
+@socketio.on("new connection")
+def on_new_connection(data):
+    for connection in db.session.query(tables.Connection).all():
+        if connection.userID1 == data["userID1"] and connection.userID2 == data["userID2"]:
+            return
+        elif connection.userID1 == data["userID2"] or connection.userID2 == data["userID1"]:
+            return
+    
+    db.session.add(tables.Connection(data["userID1"], data["userID2"]))
+    db.session.commit()
+    
+@socketio.on("query connections")
+def on_query_connections(data):
+    result = []
+    for connection in db.session.query(tables.Connection).all():
+        if connection.userID1 == data["queryUser"] or connection.userID2 == data["queryUser"]:
+            result.append(connection)
+    
+    for connection in result:
+        TODO
+    
