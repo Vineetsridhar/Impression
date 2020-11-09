40e864936645d622130cd80672ccfe7397fceca7
Vineet Sridhar <vineets1600@gmail.com>
2020-11-09 11:21:56 -0500

    Fixed all the typescript issues and code styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import styles from "../Contact/ContactsStyle.tsx";
 import { View, Text, Image, ScrollView } from "react-native";
-import { Avatar } from 'react-native-elements';
-import avatar from '../../config/avatar';
+import { Avatar } from "react-native-elements";
+import avatar from "../../config/avatar";
+import styles from "../Contact/ContactsStyle";
 
 function Contact({ name }: { name: string }) {
-   return (
-	
-	<ScrollView style={styles.container} contentContainerStyle={{ alignItems:'flex-start' }}>
-	<View style={styles.rowContainer}>
-		<Image style={styles.avatarStyle} source={{ uri: avatar }} />
-		<View style={{flexDirection: 'column'}}>
-			<Text>{name}</Text>
-			<Text>{name}</Text>
-			<Text>{name}</Text>	
-		</View>
-	</View>
-	</ScrollView>
-   );
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ alignItems: "flex-start" }}
+    >
+      <View style={styles.rowContainer}>
+        <Image style={styles.avatarStyle} source={{ uri: avatar }} />
+        <View style={{ flexDirection: "column" }}>
+          <Text>{name}</Text>
+          <Text>{name}</Text>
+          <Text>{name}</Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
 
 export default Contact;
