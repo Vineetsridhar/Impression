122778e804a18780c7dcbabd12796cdbe8b99bc4
Stephanie Nieve-Silva <smn29@njit.edu>
2020-11-10 19:55:49 +0000

    Refactor edit user, login, and connections to be rest calls

--- Backend/server.py
+++ Backend/server.py
@@ -53,13 +53,22 @@ def on_new_user():
         return {"success": False}
 
 #### Given info from a user input, changes info of user on database
-@socketio.on("edit user")
-def on_edit(data):
-    ImpUtil.Users.edit_user(data)
+# @socketio.on("edit user")
+# def on_edit(data):
+#     ImpUtil.Users.edit_user(data)
+@app.route("/edit_user", methods=["POST"])
+def on_edit():
+    data = flask.request.json
+    return ImpUtil.Users.get_user(data)
 
-@socketio.on("login")
-def on_login(data):
-    clients.append({"email": data["user_email"], "room": flask.session.get('room'), "sid": flask.request.sid}) 
+# @socketio.on("login")
+# def on_login(data):
+#     clients.append({"email": data["user_email"], "room": flask.session.get('room'), "sid": flask.request.sid}) 
+#     print(SERVER_PREFIX + str(clients[0]))
+@app.route("/login", methods=["POST"])
+def on_login():
+    data = flask.request.json
+    clients.append({"email": data["user_email"], "room": flask.session.get('room'), "sid": flask.request.sid})
     print(SERVER_PREFIX + str(clients[0]))
 
 #### Given an email, returns a dictionary with the data of the user with such an email
@@ -70,15 +79,23 @@ def get_user():
     
 #### Given 2 user emails, adds them as a new connection to the DB if such a connection does not already exist.
 #### Returns -1 if such a connection already exists, and 0 if the connection was added.
-@socketio.on("new connection")
-def on_new_connection(data):
-    ImpUtil.Connections.on_new_connection(data)
+# @socketio.on("new connection")
+# def on_new_connection(data):
+#     ImpUtil.Connections.on_new_connection(data)
+@app.route("/new_connection", methods=["POST"])
+def on_new_connection():
+    data = flask.request.json
+    return ImpUtil.Connections.on_new_connection(data)
     
 #### Given 2 user emails, remove the exisiting connection between them if it exists.
 #### Returns -1 if such a connection does not exist, and 0 if the connection existed and was deleted.
-@socketio.on("delete connection")
-def on_delete_connection(data):
-    ImpUtil.Connections.on_delete_connection(data)
+# @socketio.on("delete connection")
+# def on_delete_connection(data):
+#     ImpUtil.Connections.on_delete_connection(data)
+@app.route("/delete_connection", methods=["POST"])
+def on_delete_connection():
+    data = flask.request.json
+    return ImpUtil.Connections.on_delete_connection(data)
 
 #### Given a user X's email, returns a list of users X has a connection with. Specifically, it returns a list of dictionaries where each dictionary is the data of a user X has a connection with.
 #### Used to query for all connections involving a given user.
