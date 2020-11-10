59fe84e07fd9d2f69313d0d4cd11954c286888e6
Rami Bazoqa <rbazoqa@gmail.com>
2020-11-10 02:06:34 +0000

    modified connection database modifiers to cd B

--- Backend/server.py
+++ Backend/server.py
@@ -34,10 +34,9 @@ import tables
 
 ################################
 
-# got rid of user ID for users
-# TODO: change filter_by
-def get_user(query_user_id):
-    user = tables.User.query.filter_by(user_id=query_user_id).first()
+#### Given an email, returns a dictionary with the data of the user with such an email
+def get_user(query_user_email):
+    user = db.session.query(tables.User).filter(tables.Connection.user_email == query_user_email).first()
     response = {
         "email": user.email,
         "first_name": user.first_name,
@@ -53,42 +52,49 @@ def get_user(query_user_id):
     }
     return response
 
-# TODO: change filter_by
+#### Given 2 user emails, adds them as a new connection to the DB if such a connection does not already exist.
+#### Returns -1 if such a connection already exists, and 0 if the connection was added.
 @socketio.on("new connection")
 def on_new_connection(data):
-    for connection in db.session.query(tables.Connection) .all():
-        if (
-            connection.user_id1 == data["user_id1"]
-            and connection.user_id2 == data["user_id2"]
-        ):
-            return
-        elif (
-            connection.user_id1 == data["user_id2"]
-            or connection.user_id2 == data["user_id1"]
-        ):
-            return
-
-    db.session.add(tables.Connection(data["user_id1"], data["user_id2"]))
+    for connection in db.session.query(tables.Connection).filter((tables.Connection.user1_email == data["user1_email"]) & (tables.Connection.user2_email == data["user2_email"])).all():
+        return -1
+    for connection in db.session.query(tables.Connection).filter((tables.Connection.user1_email == data["user2_email"]) & (tables.Connection.user2_email == data["user1_email"])).all():
+        return -1
+    
+    db.session.add(tables.Connection(data["user1_email"], data["user2_email"]))
     db.session.commit()
+    return 0
 
-# TODO: change filter_by
+#### Given 2 user emails, remove the exisiting connection between them if it exists.
+#### Returns -1 if such a connection does not exist, and 0 if the connection existed and was deleted.
+@socketio.on("delete connection")
+def on_delete_connection(data):
+    for connection in db.session.query(tables.Connection).filter((tables.Connection.user1_email == data["user1_email"]) & (tables.Connection.user2_email == data["user2_email"])).all():
+        db.session.delete(connection)
+        return 0
+    for connection in db.session.query(tables.Connection).filter((tables.Connection.user1_email == data["user2_email"]) & (tables.Connection.user2_email == data["user1_email"])).all():
+        db.session.delete(connection)
+        return 0
+    
+    return -1
+
+#### Given a user X's email, returns a list of users X has a connection with. Specifically, it returns a list of dictionaries where each dictionary is the data of a user X has a connection with.
+#### Used to query for all connections involving a given user.
 @socketio.on("query connections")
 def on_query_connections(data):
     result = []
     response = []
-    for connection in db.session.query(tables.Connection).all():
-        if (
-            connection.user_id1 == data["queryUser"]
-            or connection.user_id2 == data["queryUser"]
-        ):
-            result.append(connection)
+    
+    for connection in db.session.query(tables.Connection).filter((tables.Connection.user1_email == data["user_email"]) | (tables.Connection.user2_email == data["user_email"])).all():
+        result.append(connection)
 
     for connection in result:
-        if data["queryUser"] == connection.user_id1:
-            pass
-        else:
-            pass
-    response.append({})  # UserID, Name, Description, Email, User Typ
+        if connection.user1_email == data["user_email"]:
+            response.append(get_user(connection.user1_email))
+        elif connection.user2_email == data["user_email"]:
+            response.append(get_user(connection.user1_email))
+    
+    return response
 
 @app.route("/")
 def index():
