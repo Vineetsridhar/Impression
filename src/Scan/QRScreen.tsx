131c3c1d3616abaa1634179aac70ed8c1ef20c41
Vineet Sridhar <vineets1600@gmail.com>
2020-11-11 15:57:20 -0500

    Added qr code to app

--- src/Scan/QRScreen.tsx
+++ src/Scan/QRScreen.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
-import {Image, View, Text} from 'react-native';
+import React from "react";
+import { Image, View, Text, StyleSheet } from "react-native";
+import { QRCode } from "react-native-custom-qr-codes-expo";
+import FAB from "../components/FAB";
+import { Ionicons } from "@expo/vector-icons";
+import user from "../../config/user";
 
-export default function QRScreen() {
-    return <View><Text>Mock QR</Text></View>
+export default function QRScreen({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+      <QRCode content={"impression://" + user.email} />
+
+      <FAB
+        onPress={() => {
+          navigation.push("Scan");
+        }}
+      >
+        <Ionicons
+          name="md-qr-scanner"
+          style={{ color: "white", fontSize: 30 }}
+        />
+      </FAB>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
