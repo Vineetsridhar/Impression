be92dd8fafe482acaff42bb03429a3a2bac5e499
Stephanie Nieve-Silva <smn29@njit.edu>
2020-11-13 05:04:03 +0000

    Remove unneeded function and comment a minor issue

--- tests/mocked_unittests.py
+++ tests/mocked_unittests.py
@@ -2,10 +2,11 @@ import unittest
 import unittest.mock as mock
 import sys
 
+# hardcoded directory-- fix possibly
 sys.path.insert(1, "../")
 sys.path.insert(1, "/home/ec2-user/environment/project3/Impression/Backend")
-sys.path.append('/home/ec2-user/environment/project3/Impression/Backend')
-#import pseudoapp
+sys.path.append("/home/ec2-user/environment/project3/Impression/Backend")
+# import pseudoapp
 
 import os
 from os.path import join, dirname
@@ -14,8 +15,8 @@ import flask
 import flask_sqlalchemy
 import flask_socketio
 
-import ImpUtil
-import Users
+import imp_util
+import users
 import tables
 
 KEY_INPUT = "input"
@@ -34,30 +35,14 @@ dummyUser = tables.Users(
     "image",
     "doc",
 )
-    
-#doesn't work yet without the query.filterby section
-class MockedUser():
-
-    def __init__(self, email, fname, lname, org, des, utype, gl1, gl2, gl3, im, doc):
-        self.email = email
-        self.first_name = fname
-        self.last_name = lname
-        self.organization = org
-        self.descr = des
-        self.user_type = utype
-        self.gen_link_1 = gl1
-        self.gen_link_2 = gl2
-        self.gen_link_3 = gl3
-        self.image = im
-        self.doc = doc
 
 class EditUser(unittest.TestCase):
-    
+
     def mocked_user_query_first(self, email):
         mocked_user = mock.Mock()
         mocked_user.first.return_value = dummyUser
         return mocked_user
-    
+
     def setUp(self):
         self.success_test_params = [
             {
@@ -80,12 +65,11 @@ class EditUser(unittest.TestCase):
 
     def test_edit_user_success(self):
         for test in self.success_test_params:
-            #TODO: mock 'query.filter.by' for the database
             with mock.patch('sqlalchemy.orm.query.Query.filter_by', self.mocked_user_query_first):
-                response = ImpUtil.Users.get_user(test[KEY_INPUT])
-                
+                response = imp_util.users.get_user(test[KEY_INPUT])
+
                 expected = test[KEY_EXPECTED]
-            
+
             self.assertDictEqual(response, expected)
 
 if __name__ == '__main__':
