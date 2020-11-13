2eb1acdee26447b6af6328ddacfbb15c180cdd7a
Chris <chrismazzei213@gmail.com>
2020-11-13 03:05:21 -0500

    Beautified Contact Details by adding styling for background color, font color, releative icons, and changing layout

--- src/Contact/ContactDetail.tsx
+++ src/Contact/ContactDetail.tsx
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import avatar from "../../config/avatar";
+import { Icon } from 'react-native-elements'
 
 export default function ContactDetail({ route }: { route: any }) {
   const { user } = route.params;
@@ -21,43 +22,105 @@ export default function ContactDetail({ route }: { route: any }) {
       />
       <View style={styles.infoContainer}>
       <Image
-        style={{width: 150, height: 150}}
+        style={{width: 100, height: 100}}
         source={{ uri: user["image"] || avatar }}
       />
       </View>
       <View style={styles.infoContainer}>
-          <Text style={{ fontSize: 30 }}>
+          <Text style={{ fontSize: 30, color: 'white' }}>
           {user["first_name"]} {user["last_name"]}
           </Text>
       </View>
+
       <View style={styles.rowContainer}>
           <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
-          <Text style={{textAlign: 'center'}}>
-          {user["email"]}
-          </Text>
-
+          <Icon
+          name='envelope'
+          type='font-awesome'
+          size={30}
+          color='white'
+          />
           </View>
-          <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
-          <Text style={{textAlign: 'center'}}>
-          {user["organization"] || (user["first_name"] + " is currently not enrolled in a University or involved in any Organizations.")}
+
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Text style={styles.textStyle}>
+          {user["email"]}
           </Text>
           </View>
       </View>
+
+      <View style={styles.rowContainer}>
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Icon
+          name='building'
+          type='font-awesome'
+          size={30}
+          color='white'
+          />
+          </View>
+
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Text style={styles.textStyle}>
+          {user["organization"]}
+          </Text>
+          </View>
+      </View>
+
       <View style={styles.infoContainer}>
-          <Text style={{textAlign: 'center'}}>
+          <Text style={styles.textStyle}>
           {user["descr"]}
           </Text>
       </View>
-      <View style={styles.infoContainer}>
-      <Text style={{textAlign: 'center'}}>
-      {user["gen_link_1"]}
-      </Text>
-      <Text style={{textAlign: 'center'}}>
-      {user["gen_link_2"]}
-      </Text>
-      <Text style={{textAlign: 'center'}}>
-      {user["gen_link_3"]}
-      </Text>
+
+      <View style={styles.rowContainer}>
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Icon
+          name='github'
+          type='font-awesome'
+          size={30}
+          color='white'
+          />
+          </View>
+
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Text style={styles.textStyle}>
+          {user["gen_link_1"]}
+          </Text>
+          </View>
+      </View>
+
+      <View style={styles.rowContainer}>
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Icon
+          name='linkedin'
+          type='font-awesome'
+          size={30}
+          color='white'
+          />
+          </View>
+
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Text style={styles.textStyle}>
+          {user["gen_link_2"]}
+          </Text>
+          </View>
+      </View>
+
+      <View style={styles.rowContainer}>
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Icon
+          name='link'
+          type='font-awesome'
+          size={30}
+          color='white'
+          />
+          </View>
+
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Text style={styles.textStyle}>
+          {user["gen_link_3"]}
+          </Text>
+          </View>
       </View>
       {/**Object.keys(user).map((key) => (
           <Text>
@@ -80,14 +143,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  nameContainer: {
-    flex: 1,
-    flexDirection: 'column'
+  textStyle: {
+    fontSize: 20,
+    color: 'white'
   },
   container: {
     margin: 0,
     flex: 1,
     padding: 16,
+    backgroundColor: '#192879'
   },
   icon: {
     marginVertical: 16,
