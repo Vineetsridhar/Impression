46bd9e01341c2d419239ec8a12a3d577e7677ea1
Chris <chrismazzei213@gmail.com>
2020-11-13 01:49:28 -0500

    Added basic styling for displaying Contact Details

--- src/Contact/ContactDetail.tsx
+++ src/Contact/ContactDetail.tsx
@@ -18,16 +18,75 @@ export default function ContactDetail({ route }: { route: any }) {
           navigation.navigate("Contacts");
         }}
       />
-      {Object.keys(user).map((key) => (
-        <Text>
+      <View style={styles.infoContainer}>
+          <Text>
+          Image here
+          </Text>
+      </View>
+      <View style={styles.infoContainer}>
+          <Text style={{ fontSize: 30 }}>
+          {user["first_name"]} {user["last_name"]}
+          </Text>
+      </View>
+      <View style={styles.rowContainer}>
+          <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Text style={{textAlign: 'center'}}>
+          {user["email"]}
+          </Text>
+
+          </View>
+          <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+          <Text style={{textAlign: 'center'}}>
+          testorg/school{user["organization"]}
+          </Text>
+          </View>
+      </View>
+      <View style={styles.infoContainer}>
+          <Text style={{textAlign: 'center'}}>
+          This is just a holder for now.
+          The actual description is here but is blank because it has no value yet.
+          So this is here for now. This is the same for the school/org and links.
+          {user["descr"]}
+          </Text>
+      </View>
+      <View style={styles.infoContainer}>
+      <Text style={{textAlign: 'center'}}>
+      Link1 goes here: {user["gen_link_1"]}
+      </Text>
+      <Text style={{textAlign: 'center'}}>
+      Link2 goes here: {user["gen_link_2"]}
+      </Text>
+      <Text style={{textAlign: 'center'}}>
+      Link3 goes here: {user["gen_link_3"]}
+      </Text>
+      </View>
+      {/**Object.keys(user).map((key) => (
+          <Text>
           {key}: {user[key]}
-        </Text>
-      ))}
+          </Text>
+      ))**/}
     </View>
   );
 }
 const styles = StyleSheet.create({
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rowContainer: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  nameContainer: {
+    flex: 1,
+    flexDirection: 'column'
+  },
   container: {
+    margin: 0,
     flex: 1,
     padding: 16,
   },
