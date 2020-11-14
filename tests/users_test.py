dff2b91e8061b9f253088b2e6de43ccb3e65f7e5
Rami Bazoqa <rbazoqa@gmail.com>
2020-11-14 03:20:04 +0000

    unittesting

--- tests/users_test.py
+++ tests/users_test.py
@@ -95,8 +95,20 @@ class EditUser(unittest.TestCase):
     def setUp(self):
         self.success_test_params = [
             {
-                KEY_INPUT: "",
-                KEY_EXPECTED: "Expected Error",
+                KEY_INPUT: {
+                    "email": "dummy@gmail.com",
+                    "first_name": "John",
+                    "last_name": "Smith",
+                    "organization": "Impression Co",
+                    "descr": "Description",
+                    "user_type": "Type",
+                    "gen_link_1": "link1",
+                    "gen_link_2": "link2",
+                    "gen_link_3": "link3",
+                    "image": "image",
+                    "doc": "doc",
+                },
+                KEY_EXPECTED: "No errors",
             },
         ]
 
