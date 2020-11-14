56bdbc73ea7c2e70bc366d0bb8af20e41726d71d
Stephanie Nieve-Silva <smn29@njit.edu>
2020-11-14 00:14:48 +0000

    Add file for unit test for qr.py

---
+++ tests/qr_test.py
@@ -0,0 +1,31 @@
+import unittest
+import unittest.mock as mock
+import sys
+
+# sys.path.insert(1, "../")
+sys.path.insert(1, "/home/ec2-user/environment/project3/Impression/Backend")
+sys.path.append("/home/ec2-user/environment/project3/Impression/Backend")
+
+from server import db
+import tables
+import s3
+
+import os
+import pathlib
+
+import pyqrcode
+import flask_sqlalchemy
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+class qr_test(unittest.TestCase):
+    
+    def empty1(self):
+        pass
+
+
+# with mock.patch('AWS.S3.ManagedUpload', self.empty1):
+
+if __name__ == "__main__":
+    unittest.main()
