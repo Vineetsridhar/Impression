338050621f631a5d302351d020aff5096ca5c805
Rami Bazoqa <rbazoqa@gmail.com>
2020-11-14 00:29:40 +0000

    connections_test.py complete

--- tests/connections_test.py
+++ tests/connections_test.py
@@ -42,6 +42,9 @@ dummyUser = tables.Users(
 
 class connections_tests(unittest.TestCase):
 
+    def get_mocked_user(self, value):
+        return dummyUser
+    
     def empty(self):
         pass
     
@@ -56,9 +59,15 @@ class connections_tests(unittest.TestCase):
         
     def mocked_user_query_all_empty(self):
         return []
-
+        
+    def mocked_user_query_all_1(self):
+        return [tables.Connections("dummy2@gmail.com", "dummy@gmail.com")]
+    
+    def mocked_user_query_all_2(self):
+        return [tables.Connections("dummy@gmail.com", "dummy2@gmail.com")]
+        
     def setUp(self):
-        self.success_test_params = [
+        self.on_new_connection_test_params = [
             {
                 KEY_ID: 1,
                 KEY_INPUT: {
@@ -76,15 +85,15 @@ class connections_tests(unittest.TestCase):
                 KEY_EXPECTED: "dummy2@gmail.com"
             },
             {
-                KEY_ID: 2,
+                KEY_ID: 3,
                 KEY_INPUT: {
-                    "user1_email": "dummy1@gmail.com",
-                    "user2_email": "dummy2@gmail.com",
+                    "user1_email": None,
+                    "user2_email": None,
                 },
-                KEY_EXPECTED: "dummy2@gmail.com"
+                KEY_EXPECTED: {"success": False}
             },
             {
-                KEY_ID: 3,
+                KEY_ID: 4,
                 KEY_INPUT: "dummy@gmail.com",
                 KEY_EXPECTED: {
                     "email": "dummy@gmail.com",
@@ -101,16 +110,51 @@ class connections_tests(unittest.TestCase):
                 }
             },
         ]
-
-    def test_get_user_success(self):
-        for test in self.success_test_params:
+        
+        self.on_delete_connection_test_params = [
+            {
+                KEY_ID: 1,
+                KEY_INPUT: {
+                    "user1_email": "dummy1@gmail.com",
+                    "user2_email": "dummy2@gmail.com",
+                },
+                KEY_EXPECTED: 0
+            },
+            {
+                KEY_ID: 2,
+                KEY_INPUT: {
+                    "user1_email": "dummy1@gmail.com",
+                    "user2_email": "dummy2@gmail.com",
+                },
+                KEY_EXPECTED: -1
+            },
+        ]
+        
+        self.on_query_connection_test_params = [
+            {
+                KEY_ID: 1,
+                KEY_INPUT: {
+                    "user_email": "dummy2@gmail.com",
+                },
+                KEY_EXPECTED: [self.get_mocked_user(0)]
+            },
+            {
+                KEY_ID: 1,
+                KEY_INPUT: {
+                    "user_email": "dummy@gmail.com",
+                },
+                KEY_EXPECTED: [self.get_mocked_user(0)]
+            },
+        ]
+        
+    def test_on_new_connection(self):
+        for test in self.on_new_connection_test_params:
             
             if test[KEY_ID] == 1:
                 with mock.patch('sqlalchemy.orm.query.Query.all', self.mocked_user_query_all):
                     with mock.patch('users.get_user', self.mocked_get_user):
                         with mock.patch('sqlalchemy.orm.session.Session.add', self.empty):
                             response = imp_util.connections.on_new_connection(test[KEY_INPUT])
-            
                             expected = test[KEY_EXPECTED]
         
                 self.assertEqual(response, expected)
@@ -120,8 +164,58 @@ class connections_tests(unittest.TestCase):
                     with mock.patch('users.get_user', self.mocked_get_user):
                         with mock.patch('sqlalchemy.orm.session.Session.add', self.empty_2):
                             response = imp_util.connections.on_new_connection(test[KEY_INPUT])
-            
                             expected = test[KEY_EXPECTED]
+        
+                self.assertEqual(response, expected)
+            
+            if test[KEY_ID] == 3:
+                with mock.patch('sqlalchemy.orm.query.Query.all', self.mocked_user_query_all_empty):
+                    with mock.patch('users.get_user', self.empty):
+                        with mock.patch('sqlalchemy.orm.session.Session.add', self.empty_2):
+                            response = imp_util.connections.on_new_connection(test[KEY_INPUT])
+                            expected = test[KEY_EXPECTED]
+        
+                self.assertEqual(response, expected)
+                
+    def test_delete_connection(self):
+        for test in self.on_delete_connection_test_params:
+            
+            if test[KEY_ID] == 1:
+                with mock.patch('sqlalchemy.orm.query.Query.all', self.mocked_user_query_all):
+                    with mock.patch('users.get_user', self.mocked_get_user):
+                        with mock.patch('sqlalchemy.orm.session.Session.delete', self.empty_2):
+                            with mock.patch('sqlalchemy.orm.session.Session.add', self.empty):
+                                response = imp_util.connections.on_delete_connection(test[KEY_INPUT])
+                                expected = test[KEY_EXPECTED]
+        
+                self.assertEqual(response, expected)
+            
+            if test[KEY_ID] == 2:
+                with mock.patch('sqlalchemy.orm.query.Query.all', self.mocked_user_query_all_empty):
+                    with mock.patch('users.get_user', self.mocked_get_user):
+                        with mock.patch('sqlalchemy.orm.session.Session.delete', self.empty):
+                            with mock.patch('sqlalchemy.orm.session.Session.add', self.empty):
+                                response = imp_util.connections.on_delete_connection(test[KEY_INPUT])
+                                expected = test[KEY_EXPECTED]
+            
+                self.assertEqual(response, expected)
+    
+    def test_query_connection(self):
+        for test in self.on_query_connection_test_params:
+            
+            if test[KEY_ID] == 1:
+                with mock.patch('sqlalchemy.orm.query.Query.all', self.mocked_user_query_all_1):
+                    with mock.patch('users.get_user', self.get_mocked_user):
+                        response = imp_util.connections.on_query_connections(test[KEY_INPUT])
+                        expected = test[KEY_EXPECTED]
+        
+                self.assertEqual(response, expected)
+                
+            if test[KEY_ID] == 2:
+                with mock.patch('sqlalchemy.orm.query.Query.all', self.mocked_user_query_all_1):
+                    with mock.patch('users.get_user', self.get_mocked_user):
+                        response = imp_util.connections.on_query_connections(test[KEY_INPUT])
+                        expected = test[KEY_EXPECTED]
         
                 self.assertEqual(response, expected)
 
