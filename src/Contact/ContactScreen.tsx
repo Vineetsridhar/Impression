d01aad0891b7c770031307e141ed1c21b62c0892
Chris <chrismazzei213@gmail.com>
2020-11-19 22:19:11 -0500

    Added Nav Bar for Contact Screen and Contact Details
    Nav bar for Contact Screen displays title, total Contacts connected with, and a search icon (TODO: implement search) -- Nav bar for Contact Detail Screen displays title, back arrow, and message icon (TODO: imeplment messaging)

--- src/Contact/ContactScreen.tsx
+++ src/Contact/ContactScreen.tsx
@@ -9,13 +9,20 @@ import user from "../../config/user";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import colors from '../../config/colors'
 import { FontAwesome } from "@expo/vector-icons";
+import { Appbar } from 'react-native-paper';
 
 export default function ContactScreen({ navigation }: any) {
   //Add on tab focus listener to refresh data
   const [userConnections, setUserConnections] = useState<User[]>([]);
   const [companyConnections, setCompanyConnections] = useState<User[]>([]);
 
+  const [countStudents, setCountStudents] = useState(0);
+
   let focusListener: () => {};
+
+
+  const totalConnected = "Total Contacts: " + (parseInt(userConnections.length) + parseInt(companyConnections.length));
+  const _handleSearch = () => console.log("TODO search for contact");
 
   const makeListeners = () => {
     focusListener = navigation.addListener("focus", () => {
@@ -48,6 +55,12 @@ export default function ContactScreen({ navigation }: any) {
   }, []);
   return (
     <View style={styles.container}>
+    <View style={{ width: '100%', marginTop: 15 }}>
+    <Appbar.Header>
+      <Appbar.Content title="Contacts" subtitle={totalConnected}/>
+      <Appbar.Action icon="magnify" onPress={_handleSearch} />
+    </Appbar.Header>
+    </View>
       <View style={{ flex: 2 }}>
         <FontAwesome name="building" size={60} color={colors.text} />
         <ContactList contacts={companyConnections} />
