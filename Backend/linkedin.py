7161edf21336a6a3427a1f4c55ff9b8df2a88efd
smn29 <smn29@njit.edu>
2020-11-23 17:22:23 -0500

    Add function for linkedin

---
+++ Backend/linkedin.py
@@ -0,0 +1,22 @@
+import requests
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), "secret.env")
+load_dotenv(dotenv_path)
+
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+#### Exchange auth code for access token
+def get_access_token(auth_code):
+    base_url = "https://www.linkedin.com/oauth/v2/accessToken"
+    query = "?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s" % (auth_code, REDIRECT_URI, CLIENT_ID, CLIENT_SECRET)
+    response = requests.get(base_url + query)
+    data = json.loads(response.text)
+    return data
+
+#### Getting Profile data of user
+
+#### Getting email of user
