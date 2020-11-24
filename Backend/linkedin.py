50e3d3226a7e8c5a2269e2b7221f53253daecc46
smn29 <smn29@njit.edu>
2020-11-24 16:03:43 -0500

    Add functions for getting the email and profile data of a user

--- Backend/linkedin.py
+++ Backend/linkedin.py
@@ -15,8 +15,23 @@ def get_access_token(auth_code):
     query = "?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s" % (auth_code, REDIRECT_URI, CLIENT_ID, CLIENT_SECRET)
     response = requests.get(base_url + query)
     data = json.loads(response.text)
-    return data
+    return data["access_token"]
 
 #### Getting Profile data of user
+def get_profile(access_tok):
+    base_url = "https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))"
+    response = requests.get(base_url)
+    data = json.loads(response.text)
+    user_info = {
+        "first_name": data["firstName"]["localized"]["en_US"],
+        "last_name": data["lastName"]["localized"]["en_US"],
+        "image": datat["profilePicture"]["displayImage~"]["elements"][1]["identifiers"][0]["identifier"]
+    }
+    return user_info
 
 #### Getting email of user
+def get_user_email(access_tok):
+    base_url = "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))"
+    response = requests.get(base_url)
+    data = json.loads(response.text)
+    return data["elements"][0]["handle~"]["emailAddress"]
