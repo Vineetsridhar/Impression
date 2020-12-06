65e9a3a1c0006c34cd95ef5b2a0916a11197e558
Vineet Sridhar <vineets1600@gmail.com>
2020-12-06 16:50:59 -0500

    Styled document

--- src/Contact/SharedDocuments.tsx
+++ src/Contact/SharedDocuments.tsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, ToastAndroid } from 'react-native';
+import { Text, View, ToastAndroid, StyleSheet, Linking, Alert } from 'react-native';
 import { Button } from 'react-native-paper';
-import {Document} from '../helpers/interfaces'
-import { FlatList } from 'react-native-gesture-handler';
+import { Document } from '../helpers/interfaces'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { getGroupDocuments } from '../helpers/network';
+import colors from '../../config/colors';
+import { Ionicons } from '@expo/vector-icons'; 
+import font from '../../config/font';
 
 interface props {
     navigation: any,
-    route:any
+    route: any
 }
 export default function SharedDocument({ navigation, route }: props) {
 
-    const [documents,setDocuments] = useState<Document[]>([]);
+    const [documents, setDocuments] = useState<Document[]>([]);
 
     useEffect(() => {
         getGroupDocuments(route.params.groupId).then(response => response.json()).then(json => {
-            if(json["success"]){
+            if (json["success"]) {
                 setDocuments(json["response"])
             } else {
                 ToastAndroid.show("Error getting group docs data", ToastAndroid.LONG);
@@ -23,14 +26,47 @@ export default function SharedDocument({ navigation, route }: props) {
         })
     }, [])
 
-    const documentItem = ({item}:{item:Document}) => {
-        return <Text>{item.name}</Text>
+    const openLink = (link:string) => {
+        Linking.canOpenURL(link).then((canOpenURL) => {
+            if (canOpenURL) {
+              Linking.openURL(link);
+            } else {
+              Alert.alert("Error", "Cannot open URL");
+            }
+          });
+    }
+
+    const documentItem = ({ item }: { item: Document }) => {
+        return (
+            <TouchableOpacity style={styles.item} onPress={() => openLink(item.url)}>
+                <Ionicons name="md-cloud-download" size={40} color={colors.main} />
+                <Text style={styles.text}>{item.name}</Text>
+            </TouchableOpacity>
+        )
     }
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList
                 data={documents}
-                renderItem={documentItem}/>
+                renderItem={documentItem} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:colors.background,
+        padding:16,
+        flex:1
+    },
+    item:{
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    text:{
+        fontFamily: font.regular,
+        fontSize: 20,
+        paddingHorizontal: 16
+    }
+})
