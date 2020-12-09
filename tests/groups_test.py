80b40222e5d5534c098324392701582d7c93da12
smn29 <smn29@njit.edu>
2020-12-09 01:47:13 +0000

    Add unit tests for groups

---
+++ tests/groups_test.py
@@ -0,0 +1,269 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=unused-import
+# pylint: disable=no-self-use
+# pylint: disable=wrong-import-position
+
+import unittest
+import unittest.mock as mock
+import sys
+
+sys.path.insert(1, "/home/ec2-user/environment/project3/Impression/Backend")
+sys.path.append("/home/ec2-user/environment/project3/Impression/Backend")
+
+import imp_util
+import groups
+import tables
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+KEY_GROUPID = "0"
+KEY_EMAIL = "email"
+KEY_GROUPNAME = "name"
+
+dummyGroup = tables.Groups(
+    "1",
+    "dummy@gmail.com",
+    "psuedogroup",
+)
+
+
+#### GET groups test
+class GetGroups(unittest.TestCase):
+    def mocked_group_query_all(self, email):
+        mocked_group = mock.Mock()
+        mocked_group.all.return_value = dummyGroup
+        return mocked_group
+    
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: "dummy@gmail.com",
+                KEY_EXPECTED: {
+                    "success": True,
+                    "response": {
+                        KEY_GROUPID: "1",
+                        KEY_EMAIL: "dummy@gmail.com",
+                        KEY_GROUPNAME: "psuedogroup",
+                    }
+                },
+                KEY_INPUT: "dummy123@gmail.com",
+                KEY_EXPECTED: {
+                    "success": False, 
+                },
+                KEY_INPUT: None,
+                KEY_EXPECTED: {
+                    "success": False
+                    
+                },
+            },
+        ]
+
+    def test_get_user_success(self):
+        for test in self.success_test_params:
+            with mock.patch(
+                "sqlalchemy.orm.query.Query.all", self.mocked_group_query_all
+            ):
+                response = imp_util.groups.get_groups(test[KEY_INPUT])
+
+                expected = test[KEY_EXPECTED]
+
+            self.assertDictEqual(response, expected)
+
+#### GET users test
+#### FINISH
+class GetUsers(unittest.TestCase):
+    def mocked_group_query_all(self, name, email):
+        mocked_group = mock.Mock()
+        mocked_group.all.return_value = dummyGroup
+        return mocked_group
+    
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: "pseudogroup",
+                KEY_EXPECTED: {
+                    "success": True,
+                    "response": {
+                        
+                    }
+                },
+                KEY_INPUT: "groupname",
+                KEY_EXPECTED: {
+                    "success": "False", 
+                },
+                KEY_INPUT: None,
+                KEY_EXPECTED: {
+                    "success": False
+                    
+                },
+            },
+        ]
+
+    def test_get_user_success(self):
+        for test in self.success_test_params:
+            with mock.patch(
+                "sqlalchemy.orm.query.Query.all", self.mocked_group_query_all
+            ):
+                response = imp_util.users.get_user(test[KEY_INPUT])
+
+                expected = test[KEY_EXPECTED]
+
+            self.assertDictEqual(response, expected)
+
+#### NEW GROUP test
+#### FINISH
+class NewGroup(unittest.TestCase):
+    #sqlalchemy.sql.expression.desc
+    #sqlalchemy.orm.Query.order_by
+    def mocked_add(self):
+        pass
+
+    def mocked_order_by(self):
+        pass
+
+    def mocked_desc(self):
+        pass
+
+    def mocked_user_query_first(self, name):
+        mocked_user = mock.Mock()
+        mocked_user.first.return_value = dummyGroup
+        return mocked_user
+
+    def mocked_user_query_all(self, name):
+        mocked_user = mock.Mock()
+        mocked_user.all.return_value = dummyGroup
+        return mocked_user
+    
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: {
+                    KEY_EMAIL: "dummy@gmail.com",
+                    KEY_GROUPNAME: "pseudogroup",
+                },
+                KEY_EXPECTED: {
+                    "success": True,
+                },
+                KEY_INPUT: None,
+                KEY_EXPECTED: {
+                    "success": False
+                    
+                },
+            },
+        ]
+
+    def test_get_user_success(self):
+        for test in self.success_test_params:
+            with mock.patch(
+                'sqlalchemy.orm.query.Query.filter_by', self.mocked_group_query_first
+            ):
+                with mock.patch(
+                    'sqlalchemy.orm.Query.order_by', self.mocked_order_by
+                ):
+                    with mock.patch(
+                        "sqlalchemy.orm.session.Session.add", self.mocked_add
+                    ):
+                        with mock.patch(
+                            "sqlalchemy.sql.expression.desc", self.mocked_desc
+                        ):
+                            response = imp_util.groups.leave_group(test[KEY_INPUT])
+                            expected = test[KEY_EXPECTED]
+            self.assertDictEqual(response, expected)
+
+#### ADD USER test
+class AddUser(unittest.TestCase):
+    def mocked_add(self):
+        pass
+
+    def mocked_order_by(self):
+        pass
+
+    def mocked_desc(self):
+        pass
+
+    def mocked_group_query_first(self, g_id, email, name):
+        mocked_user = mock.Mock()
+        mocked_user.first.return_value = dummyGroup
+        return mocked_user
+    
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: {
+                    KEY_EMAIL: "dummy@gmail.com",
+                    KEY_GROUPNAME: "pseudogroup",
+                },
+                KEY_EXPECTED: {
+                    "success": True,
+                },
+                KEY_INPUT: None,
+                KEY_EXPECTED: {
+                    "success": False
+                    
+                },
+            },
+        ]
+
+    def test_get_user_success(self):
+        for test in self.success_test_params:
+            with mock.patch(
+                'sqlalchemy.orm.query.Query.filter_by', self.mocked_group_query_first
+            ):
+                with mock.patch(
+                    'sqlalchemy.orm.Query.order_by', self.mocked_order_by
+                ):
+                    with mock.patch(
+                        "sqlalchemy.orm.session.Session.add", self.mocked_add
+                    ):
+                        with mock.patch(
+                            "sqlalchemy.sql.expression.desc", self.mocked_desc
+                        ):
+                            response = imp_util.groups.leave_group(test[KEY_INPUT])
+                            expected = test[KEY_EXPECTED]
+            self.assertDictEqual(response, expected)
+
+#### LEAVE GROUP test
+class LeaveGroup(unittest.TestCase):
+    def mocked_delete(self):
+        pass
+
+    def mocked_group_query_first(self, g_id, email, name):
+        mocked_user = mock.Mock()
+        mocked_user.first.return_value = dummyGroup
+        return mocked_user
+    
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: {
+                    KEY_GROUPID: "1",
+                    KEY_EMAIL: "dummy@gmail.com",
+                    KEY_GROUPNAME: "pseudogroup",
+                },
+                KEY_EXPECTED: {
+                    "success": True,
+                },
+                KEY_INPUT: None,
+                KEY_EXPECTED: {
+                    "success": False
+                    
+                },
+            },
+        ]
+
+    def test_get_user_success(self):
+        for test in self.success_test_params:
+            with mock.patch(
+                'sqlalchemy.orm.query.Query.filter_by', self.mocked_group_query_first
+            ):
+                with mock.patch( 'sqlalchemy.orm.session.Session.delete', self.mocked_delete
+                    ):
+                    response = imp_util.groups.leave_group(test[KEY_INPUT])
+                    expected = test[KEY_EXPECTED]
+            self.assertDictEqual(response, expected)
+
+if __name__ == "__main__":
+    unittest.main()
